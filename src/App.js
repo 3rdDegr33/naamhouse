990be3993ffced0bf750c9b259483f9882d08f0c
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-15 17:17:09 -0500

    adding yelp api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const BEARER_TOKEN = 'i6q8cfgpqWfFopwlLkT9P1P_C8uyeIGG5kKXqKpXXIc6ng6lAe4qPsZVDptMWMF4f1pjLn8oIh8uKHy3BmfMbbyPs3_yoHMdJHnDeGzs3eC8YILO6r-y5oTBfFS6YXYx';
+  function fetchReq(){
+    // const options = {
+    //   headers: {
+    //     'Authorization': `Bearer ${BEARER_TOKEN}`,
+    //     'Origin': 'localhost:3000',
+    //     'withCredentials': true,
+    //   }
+    // }
+    // fetch(`https://api.yelp.com/v3/businesses/search` ,  {
+    //   headers: {
+    //     Authorization: `Bearer ${BEARER_TOKEN}`,
+    //     withCredentials: true,
+    //     "Access-Control-Allow-Origin": "*",
+    //     'Content-Type':  'application/json'
+    //   },
+    //   params: {
+    //     term: 'food',
+    //     location: 'NYC'
+      
+    // }})
+    // .then(data => data.json())
+    // .then(resp =>  console.log("resp",resp))
+   axios.get(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search` , {
+      headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`,
+            withCredentials: true,
+            "Access-Control-Allow-Origin": "*",
+            'Content-Type':  'application/json'
+          }, params: {
+                term: 'food',
+                location: 'NYC'
+              
+            }
+
+    }).then(({data})=>console.log(data) )
+
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +56,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={fetchReq}>fetch</button>
       </header>
     </div>
   );
