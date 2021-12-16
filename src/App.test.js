3d396065b149fa26e4b42ef944395a166590453e
Johnny Moya <profjmoya@gmail.com>
2021-12-16 15:54:11 -0600

    Navigation Bar Added

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-// Possibly delete this file? Ask.
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">NaamHouse</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="apply">Apply</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="events">Events</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about us">About Us</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="get involved">Get Involved</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="sign up">Sign Up</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
