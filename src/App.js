1d9a746d1c6d582277625f030bc7824dbb1283e9
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-16 21:30:28 -0500

    adding navigation control

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ function App() {
   const TOKEN = process.env.REACT_APP_BEARER_TOKEN;
 
   useEffect(()=>{
-    axios.get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search" , {  
+    axios.get(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search`, {  
       headers: {
             Authorization: `Bearer ${TOKEN}`,
             withCredentials: true,
             "Access-Control-Allow-Origin": "*",
-            'Content-Type':  'application/json'
           }, params: {
                 term: 'food',
                 location: 'NYC'
--- src/navigation/Map.js
+++ src/navigation/Map.js
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL , { Marker } from 'react-map-gl';
-
+import ReactMapGL , { Marker  , NavigationControl} from 'react-map-gl';
+// import { BoxIconElement } from 'boxicons';
 function Map() {
   const map_key =   process.env.REACT_APP_MAP_KEY
   const [viewport, setViewport] = useState({
     width :400,
     height :400,
-    longitude: -74.1,
-    latitude: 40.7,
+    longitude: -77.03631965305111,
+    latitude: 38.898013005557324,
     zoom: 14,
     bearing: 0,
     pitch: 0
   });
-
+  const navControlStyle= {
+    right: 10,
+    top: 10
+  };
   return (
       <>
     <ReactMapGL
@@ -21,14 +24,17 @@ function Map() {
       mapStyle={'mapbox://styles/mapbox/streets-v9'}
       mapboxApiAccessToken={ map_key}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-      >
+      >   <NavigationControl style={navControlStyle} />
           <Marker
            latitude={38.898013005557324}
            longitude={-77.03631965305111}
            offsetLeft={-20}
            offsetTop={-10}
-        
-          />
+         
+           >  
+           <i className='bx bxs-map'style={{fontSize: `${viewport.zoom * 0.2}rem`}}></i>
+            
+        </Marker>
        </ReactMapGL>
       </>
   )
