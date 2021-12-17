3f9a32f50559b2c5d446252c77ea6a035c21b2e0
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-17 12:56:45 -0500

    adding mock user location

--- src/navigation/Map.js
+++ src/navigation/Map.js
@@ -30,7 +30,13 @@ function Map({center}) {
       onViewportChange={nextViewport => setViewport(nextViewport)}
       maxZoom={18}
       >   <NavigationControl style={navControlStyle} />
-         
+           <Marker
+              longitude = {-73.96892359984999}
+              latitude = {40.778856033719066}
+           >
+           <span className='i-pin-card-map mapboxgl-marker mapboxgl-marker-anchor-center location' >
+                   </span>
+           </Marker>
              {center.map((e , i) =>(
                <Marker key={i}
                 latitude={e.attributes.lat}
