be521c2fef04f0e62b79ebbd75b905abc2cad5eb
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-17 11:17:59 -0500

    adding dynamic markers

--- src/navigation/Map.js
+++ src/navigation/Map.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { useState } from 'react';
 import mapboxgl from "mapbox-gl";
-
 import ReactMapGL , { Marker  , NavigationControl , Layer} from 'react-map-gl';
 // import { BoxIconElement } from 'boxicons';
 function Map({center}) {
   const map_key =   process.env.REACT_APP_MAP_KEY
   const [viewport, setViewport] = useState({
-    width :400,
-    height :400,
+    width :695,
+    height :694,
     longitude: -77.03631965305111,
     latitude: 38.898013005557324,
     zoom: 14,
     bearing: 0,
     pitch: 0
+    
   });
   const navControlStyle= {
     right: 10,
@@ -29,25 +29,18 @@ function Map({center}) {
       onViewportChange={nextViewport => setViewport(nextViewport)}
       maxZoom={18}
       >   <NavigationControl style={navControlStyle} />
-          <Marker
-           latitude={38.898013005557324}
-           longitude={-77.03631965305111}
-           offsetLeft={-20}
-           offsetTop={-10}
          
-           >  
-           <i className='bx bxs-map'style={{fontSize: `${viewport.zoom * 0.2}rem`}}></i>
-           </Marker>
-     {/* <Layer
-      id = 'snotel-sites-circle'
-      type= 'circle'
-      source = "snotel-sites"
-      paint={{ "circle-color": "#1d1485", "circle-radius": 8,
-      "circle-stroke-color": "#ffffff",
-      "circle-stroke-width": 2}}
-      latitude={38.898013005557324}
-      longitude={-77.03631965305111}
-     /> */}
+             {center.map((e , i) =>(
+               <Marker key={i}
+                latitude={e.attributes.lat}
+                longitude={e.attributes.long}
+                offsetTop={-22}
+               >
+                   <span className='i-pin-card-map mapboxgl-marker mapboxgl-marker-anchor-center'>
+                     A
+                   </span>
+               </Marker>
+             ))}
       
     
        </ReactMapGL>
